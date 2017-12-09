2ba79ecf5563a1b25b1ee7dec4ffbfdfe1665c7a
Unknown <angelamdeveloper@gmail.com>
2017-12-09 21:55:21 +0100

    video 51

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+// Servicios
+import { HeroesService} from './servicios/heroes.service';
+
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -16,9 +26,14 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
