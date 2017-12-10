5e08ad003bbca1e0a84441cd55e7bde6992c4ea1
Unknown <angelamdeveloper@gmail.com>
2017-12-10 22:22:19 +0100

    video 56

--- src/app/servicios/heroes.service.ts
+++ src/app/servicios/heroes.service.ts
@@ -59,7 +59,11 @@ export class HeroesService {
 
 	getHeroes():Heroe[]{
     	return this.heroes;
-  	}
+  }
+
+  getHeroe( idx: string ){
+    return this.heroes[idx];
+  }
 
 }
 export interface Heroe{
